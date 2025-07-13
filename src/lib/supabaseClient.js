bc83c205551fc59ca3aba2889ad06d39e1233294
NaveenapriyaN <naveenapriya13@gmail.com>
2025-07-13 16:43:24 +0530

    Add console log to debug Supabase URL

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("💡 Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("💡 Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
